bc82a662f5806f9140874026406dbfb290d9cb82
Conor-Miller9276 <Miller-C14@ulster.ac.uk>
2025-10-27 00:53:14 +0000

    deatfef

--- Assets/_Game/Code/Hitbox.cs
+++ Assets/_Game/Code/Hitbox.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Hitbox : MonoBehaviour
+public class FallenKnight : MonoBehaviour
 
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
